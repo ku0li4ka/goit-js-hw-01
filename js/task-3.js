73914a903c2218c08cb282c8cd9a5249eb5921ea
Olha Sobshynska <ku.0li4ka@gmail.com>
2024-01-04 17:49:35 +0200

    Update task-3
    remark

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function getElementWidth(content, padding, border) {
   const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
-  const bordertWidth = parseFloat(border);
-  const totalWidth = contentWidth + paddingWidth * 2 + bordertWidth * 2;
+  const borderWidth = parseFloat(border);
+  const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
   return totalWidth;
 }
 
